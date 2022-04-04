d58f8dea131fbe79264c00321f475a50da12e26d
Rush <kriscookies20@gmail.com>
2022-04-03 21:11:06 -0700

    included html info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 var startButtonEl = document.getElementById("start");
 console.log(startButtonEl);
 function startQuiz() {
-    var questionSection = document.querySelector(".questions")
-    questionSection.classList.remove("questions")
+    var questionSection = document.querySelector(".question-one")
+    questionSection.classList.remove("question-one")
     var homePage = document.querySelector(".startpage")
     console.log(homePage)
     homePage.style.display = "none"
